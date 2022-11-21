2b71e45f5935ae636253c4c11e10ea9fc8222e62
yosuke ota <otameshiyo23@gmail.com>
2022-11-22 07:35:56 +0900

    Add support for satisfies operators

---
+++ tests/fixtures/ts/satisfies-operators.ts
@@ -0,0 +1,9 @@
+type Colors = "red" | "green" | "blue";
+
+type RGB = [red: number, green: number, blue: number];
+
+const palette = {
+    red: [255, 0, 0],
+    green: "#00ff00",
+    blue: [0, 0, 255]
+} satisfies Record<Colors, string | RGB>;
